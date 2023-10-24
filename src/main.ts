66f7ba9b6e6e262242be6e9431b617e44385bf13
higor <hgrprofissional@gmail.com>
2023-10-24 15:11:36 -0300

    refactor: prefixo api

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,15 @@ import { ValidationPipe } from '@nestjs/common/pipes';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe());
   const config = new DocumentBuilder()
     .setTitle('Alunos API')
     .setDescription('API para gerenciar cadastro de Alunos')
     .setVersion('1.0')
-    .addTag('alunos')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api/docs', app, document);
 
   await app.listen(3000);
 }
